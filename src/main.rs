400194e4d91a508f90cc13e4ee16dc1dc1110561
9motom6 <9motom6@gmail.com>
2020-12-29 15:46:22 +0100

    POST hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use reqwest::blocking;
+use reqwest::Error;
+
+fn main() -> Result<(), Error> {
+    let body = reqwest::blocking::get("https://www.rust-lang.org")?.text()?;
+
+
+    println!("body = {:?}", body);
+    Ok(())
 }
