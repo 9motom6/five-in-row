eed92b848e0dcda4132b39d130ad8409b163afee
9motom6 <9motom6@gmail.com>
2020-12-29 16:12:21 +0100

    CREATE user

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,29 @@
 use reqwest::blocking;
+use reqwest::blocking::Response;
 use reqwest::Error;
+use serde::Serialize;
+use std::convert::TryInto;
+
+#[derive(Serialize)]
+struct User {
+    nickname: String,
+    email: String,
+}
 
 fn main() -> Result<(), Error> {
-    let body = reqwest::blocking::get("https://www.rust-lang.org")?.text()?;
+    create_user();
+    Ok(())
+}
 
+fn create_user() -> Result<(), Error> {
+    let user: User = User { nickname: String::from("9motom6"), email: String::from("9motom6@gmail.com") };
+    let client = reqwest::blocking::Client::new();
 
-    println!("body = {:?}", body);
+    let res: String = client.post("https://piskvorky.jobs.cz/api/v1/user")
+        .body(serde_json::to_string(&user).unwrap())
+        .send()?
+        .text()?;
+    println!("{}", res);
+
     Ok(())
 }
